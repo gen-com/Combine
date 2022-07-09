501f16d622dc759c7de47661b7abbb7c7aa5cbe7
Koo <31850425+gen-com@users.noreply.github.com>
2022-07-09 20:01:07 +0900

    Feat Supporting files

---
+++ WizardingSchool-UIKit/WizardingSchool-UIKit/Supporting files/SceneDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  SceneDelegate.swift
+//  WizardingSchool-UIKit
+//
+//  Created by Byeongjo Koo on 2022/07/02.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+}
+
